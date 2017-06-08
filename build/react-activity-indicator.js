6dc802593b2e42aee84feadef299aa659ab651f9
Keisuke Kan <9renpoto@gmail.com>
2017-06-08 19:13:45 +0900

    build(build)

--- build/react-activity-indicator.js
+++ build/react-activity-indicator.js
@@ -7,7 +7,7 @@
 		exports["ActivityIndicator"] = factory(require("react"));
 	else
 		root["ActivityIndicator"] = factory(root["React"]);
-})(this, function(__WEBPACK_EXTERNAL_MODULE_1__) {
+})(this, function(__WEBPACK_EXTERNAL_MODULE_0__) {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -16,21 +16,21 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	function __webpack_require__(moduleId) {
 /******/
 /******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
+/******/ 		if(installedModules[moduleId]) {
 /******/ 			return installedModules[moduleId].exports;
-/******/
+/******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
 /******/ 		};
 /******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/
 /******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
+/******/ 		module.l = true;
 /******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
@@ -43,131 +43,157 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
 /******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "build/";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-	'use strict';
-	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	exports.default = undefined;
-	
-	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-	
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-	
-	var _react = __webpack_require__(1);
-	
-	var _react2 = _interopRequireDefault(_react);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-	
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-	
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-	
-	var ActivityIndicator = function (_Component) {
-	  _inherits(ActivityIndicator, _Component);
-	
-	  function ActivityIndicator(props) {
-	    _classCallCheck(this, ActivityIndicator);
-	
-	    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(ActivityIndicator).call(this, props));
-	
-	    _this.state = { duration: _this.props.duration || 400 };
-	    _this.state.number = props.number || 3;
-	    return _this;
-	  }
-	
-	  _createClass(ActivityIndicator, [{
-	    key: 'componentDidMount',
-	    value: function componentDidMount() {
-	      var _this2 = this;
-	
-	      this.interval = setInterval(function () {
-	        var active = _this2.state.active;
-	        active = active >= _this2.state.number || isNaN(_this2.state.active) ? 0 : active + 1;
-	        _this2.setState({ active: active });
-	      }, this.state.duration * 2);
-	    }
-	  }, {
-	    key: 'componentWillUnmount',
-	    value: function componentWillUnmount() {
-	      clearInterval(this.interval);
-	    }
-	  }, {
-	    key: 'render',
-	    value: function render() {
-	      var _this3 = this;
-	
-	      var diameter = this.props.diameter || 20;
-	      diameter = parseInt(diameter);
-	      var acColor = this.props.activeColor || "black";
-	      var n = this.props.number || 3;
-	      var indicatorStyle = {
-	        width: diameter,
-	        height: diameter,
-	        borderRadius: this.props.borderRadius || '10%',
-	        borderWidth: typeof this.props.borderWidth != "undefined" ? this.props.borderWidth : 1,
-	        borderStyle: 'solid',
-	        background: this.props.color || "transparent",
-	        transitionDuration: this.state.duration,
-	        display: 'inline-block',
-	        lineHeight: diameter,
-	        fontSize: 0,
-	        borderColor: this.props.borderColor || acColor
-	      };
-	      var containerWidth = n * diameter * 2 - diameter;
-	      return _react2.default.createElement(
-	        'div',
-	        { className: 'activity-indicator' },
-	        _react2.default.createElement(
-	          'div',
-	          { style: { width: containerWidth } },
-	          function () {
-	            var children = [],
-	                className = void 0;
-	            for (var i = 0; i < _this3.state.number; i++) {
-	              if (_this3.state.active == i) {
-	                indicatorStyle.background = acColor;
-	              } else {
-	                indicatorStyle.background = _this3.props.color || "transparent";
-	              }
-	              children.push(_react2.default.createElement('div', { key: "indicator_" + i, style: _extends({
-	                  marginLeft: i > 0 ? diameter : 0
-	                }, indicatorStyle), className: className }));
-	            }
-	            return children;
-	          }()
-	        )
-	      );
-	    }
-	  }]);
-	
-	  return ActivityIndicator;
-	}(_react.Component);
-	
-	exports.default = ActivityIndicator;
+module.exports = __WEBPACK_EXTERNAL_MODULE_0__;
 
-/***/ },
+/***/ }),
 /* 1 */
-/***/ function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	module.exports = __WEBPACK_EXTERNAL_MODULE_1__;
+"use strict";
 
-/***/ }
-/******/ ])
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
 });
-;
+exports.default = undefined;
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var ActivityIndicator = function (_Component) {
+  _inherits(ActivityIndicator, _Component);
+
+  function ActivityIndicator(props) {
+    _classCallCheck(this, ActivityIndicator);
+
+    var _this = _possibleConstructorReturn(this, (ActivityIndicator.__proto__ || Object.getPrototypeOf(ActivityIndicator)).call(this, props));
+
+    _this.state = { duration: _this.props.duration || 400 };
+    _this.state.number = props.number || 3;
+    return _this;
+  }
+
+  _createClass(ActivityIndicator, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      var _this2 = this;
+
+      this.interval = setInterval(function () {
+        var active = _this2.state.active;
+        active = active >= _this2.state.number || isNaN(_this2.state.active) ? 0 : active + 1;
+        _this2.setState({ active: active });
+      }, this.state.duration * 2);
+    }
+  }, {
+    key: 'componentWillUnmount',
+    value: function componentWillUnmount() {
+      clearInterval(this.interval);
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var _this3 = this;
+
+      var diameter = this.props.diameter || 20;
+      diameter = parseInt(diameter);
+      var acColor = this.props.activeColor || "black";
+      var n = this.props.number || 3;
+      var indicatorStyle = {
+        width: diameter,
+        height: diameter,
+        borderRadius: this.props.borderRadius || '10%',
+        borderWidth: typeof this.props.borderWidth != "undefined" ? this.props.borderWidth : 1,
+        borderStyle: 'solid',
+        background: this.props.color || "transparent",
+        transitionDuration: this.state.duration,
+        display: 'inline-block',
+        lineHeight: diameter,
+        fontSize: 0,
+        borderColor: this.props.borderColor || acColor
+      };
+      var containerWidth = n * diameter * 2 - diameter;
+      return _react2.default.createElement(
+        'div',
+        { className: 'activity-indicator' },
+        _react2.default.createElement(
+          'div',
+          { style: { width: containerWidth } },
+          function () {
+            var children = [],
+                className = void 0;
+            for (var i = 0; i < _this3.state.number; i++) {
+              if (_this3.state.active == i) {
+                indicatorStyle.background = acColor;
+              } else {
+                indicatorStyle.background = _this3.props.color || "transparent";
+              }
+              children.push(_react2.default.createElement('div', { key: "indicator_" + i, style: _extends({
+                  marginLeft: i > 0 ? diameter : 0
+                }, indicatorStyle), className: className }));
+            }
+            return children;
+          }()
+        )
+      );
+    }
+  }]);
+
+  return ActivityIndicator;
+}(_react.Component);
+
+exports.default = ActivityIndicator;
+
+/***/ })
+/******/ ]);
+});
 //# sourceMappingURL=react-activity-indicator.map
